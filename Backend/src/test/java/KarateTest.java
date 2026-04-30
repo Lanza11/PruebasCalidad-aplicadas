e86b4b7c6d6be42a890c87d596a65c4f8505eacb
Mariana <mariana.carvajalr@udea.edu.co>
2026-04-30 11:14:38 -0500

    Reduciendo deuda técnica

--- Backend/src/test/java/KarateTest.java
+++ Backend/src/test/java/KarateTest.java
@@ -1,8 +1,7 @@
-
 import com.intuit.karate.junit5.Karate;
-import org.junit.jupiter.api.Test;
 
 public class KarateTest {
+
     @Karate.Test
     Karate testAll() {
         return Karate.run("classpath:karate").relativeTo(getClass());
